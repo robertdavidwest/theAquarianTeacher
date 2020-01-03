4bdc1b444d6bd787ba0286d2640679df64d3d1aa
Robert West <robert.david.west@gmail.com>
2020-01-03 13:21:09 -0500

    added chapters json and a new route for every chapter in the json, added links to the chapters.ejs page

--- app.js
+++ app.js
@@ -7,18 +7,37 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+
+function loadChapters(){
+  const rawChapters = fs.readFileSync("chapters.json");
+  const chapters = JSON.parse(rawChapters);
+  
+  const rawHist = fs.readFileSync("history.json");
+  const hist = JSON.parse(rawHist);
+  
+  chapters.forEach(function(chapter){
+      if (chapter.shortname === 'c4_history') {
+          chapter['data'] = hist;
+      }
+  })
+  return chapters;
+}
+const chapters = loadChapters();
+
+
 app.get("/", function(req, res){
     res.render("home");
 });
 
 app.get("/chapters", function(req, res){
-    res.render("chapters");
+    res.render("chapters", {chapters: chapters});
 });
 
-app.get("/4/a-brief-history-of-yoga", function(req, res){
-    let rawHist = fs.readFileSync("history.json");
-    let hist = JSON.parse(rawHist);
-    res.render("c4_history", {data: hist});
-});
+chapters.forEach(function(chapter){
+    const route = `/chapters/${chapter.number}/${chapter.routeName}`;
+    app.get(route, function(req, res){
+        res.render(chapter.shortname, {data: chapter.data});
+    });
+})
 
 app.listen(3000);
